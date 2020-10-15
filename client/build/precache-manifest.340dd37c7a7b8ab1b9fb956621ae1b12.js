127a4cf9ceb6aa4cfd043242290b6e0b025eca4e
Jim Hopkinson <jahopki@hotmail.com>
2020-10-15 03:36:24 -0600

    2ndrunbuild

---
+++ client/build/precache-manifest.340dd37c7a7b8ab1b9fb956621ae1b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c52372cf8ffbbc64df9533d6c64498c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d144bb81e41a4590a47e",
+    "url": "/static/css/main.59b08484.chunk.css"
+  },
+  {
+    "revision": "503833b8789352b281b3",
+    "url": "/static/js/2.ad926008.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.ad926008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d144bb81e41a4590a47e",
+    "url": "/static/js/main.921dc70d.chunk.js"
+  },
+  {
+    "revision": "1612dab30dd87dad723c",
+    "url": "/static/js/runtime-main.9fc5f041.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
